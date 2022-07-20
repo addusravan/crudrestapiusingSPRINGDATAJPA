eead9e6407bf180554d6216b7b50d24fa3aca20b
addusravan <49831534+addusravan@users.noreply.github.com>
2022-07-20 12:26:40 +0530

    Add files via upload

---
+++ springdatajpacrudd/src/main/java/com/baseexample/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.baseexample.springboot.cruddemo.dao;
+
+import com.baseexample.springboot.cruddemo.entity.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ springdatajpacrudd/src/main/java/com/baseexample/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.baseexample.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.baseexample.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	
+
+	public List<Employee>FindAll();
+	public Employee findbyid(int theId);
+	public void save(Employee theEmployee);
+	public void DeleteEmployee(int theId);
+	
+
+	
+
+}
